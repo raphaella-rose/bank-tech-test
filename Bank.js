2fe64772edf10213c7975f194d114010ae13ae80
raphaella-rose <raphaella.rose1@gmail.com>
2022-07-25 10:59:21 +0100

    refactored deposit function

--- Bank.js
+++ Bank.js
@@ -8,14 +8,7 @@ class Bank {
   }
 
   deposit(amount) {
-    if (amount == 5) {
-      this.balance += 5
-    } else if (amount == 10) {
-      this.balance += 10
-    } else {
-      this.balance += 20;
-    }
-    
+    this.balance += amount;
   }
 }
 
